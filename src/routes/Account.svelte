c0005232c92e1e0c580aefcb15ae07283d356279
Nick Castle <castle5896@gmail.com>
2023-02-23 13:36:54 -0500

    create account component

---
+++ src/routes/Account.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { AuthSession } from '@supabase/supabase-js';
+	import { supabase } from '$lib/supabaseClient';
+
+	export let session: AuthSession;
+
+	let loading = false;
+	let username: string | null = null;
+	let website: string | null = null;
+	let avatarUrl: string | null = null;
+
+	onMount(() => {
+		getProfile();
+	});
+
+	const getProfile = async () => {
+		try {
+			loading = true;
+			const { user } = session;
+
+			const { data, error, status } = await supabase
+				.from('profiles')
+				.select(`username, website, avatar_url`)
+				.eq('id', user.id)
+				.single();
+
+			if (data) {
+				username = data.username;
+				website = data.website;
+				avatarUrl = data.avatar_url;
+			}
+
+			if (error && status !== 406) throw error;
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
+	};
+
+	async function updateProfile() {
+		try {
+			loading = true;
+			const { user } = session;
+
+			const updates = {
+				id: user.id,
+				username,
+				website,
+				avatarUrl,
+				updated_at: new Date()
+			};
+
+			let { error } = await supabase.from('profiles').upsert(updates);
+			if (error) throw error;
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
+	}
+
+	async function signOut() {
+		try {
+			loading = true;
+			let { error } = await supabase.auth.signOut();
+			if (error) throw error;
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
+	}
+</script>
+
+<form class="form-widget" on:submit|preventDefault={updateProfile}>
+	<div>
+		<label for="email">Email</label>
+		<input id="email" type="text" value={session.user.email} disabled />
+	</div>
+	<div>
+		<label for="username">Name</label>
+		<input id="username" type="text" bind:value={username} />
+	</div>
+	<div>
+		<label for="website">Website</label>
+		<input id="website" type="text" bind:value={website} />
+	</div>
+
+	<div>
+		<input class="button block primary" type="submit" value={loading ? 'Loading' : 'Update'} />
+	</div>
+
+	<div>
+		<button class="button block" on:click={signOut} disabled={loading}>Sign Out</button>
+	</div>
+</form>
